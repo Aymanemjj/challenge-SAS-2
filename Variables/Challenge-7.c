607161fa6b424c22bafc146990072faa23b05be2
Aymanemjj <aymanemj588@gmail.com>
2025-09-23 12:08:24 +0100

    fix Challenge 7 : Moyenne pondérée de trois nombres

--- Variables/Challenge-7.c
+++ Variables/Challenge-7.c
@@ -6,7 +6,7 @@ float number1, number2, number3,sum;
 printf("Insert 3 numbers:\n");
 scanf("%f %f %f", &number1, &number2, &number3);
 
-sum = (number1 * 2 + number2 * 3 + number3 *5) / 8;
+sum = (number1 * 2 + number2 * 3 + number3 *5) / 10;
 
 printf("%.3f\n", sum);
 return 0;
